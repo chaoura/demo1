b3a83aa440efdf3ddcc78ff8a51d4e03b5f97dbe
chaoura <chaoura93@gmail.com>
2025-07-05 18:15:57 +0100

    Mise à jour du code après modifications

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
-package com.example.controller;
+package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class HomeController {
 
 
     }
-    @RequestMapping ("/p1")
+    @RequestMapping ("/p2")
 
     public String m2(){
         return "welcome to tanger";
